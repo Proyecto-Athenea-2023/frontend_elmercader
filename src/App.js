2b85c2d8acb50157b8e13701902aca90566a7d95
EngAndres <jerarquia04@gmail.com>
2023-07-21 08:13:48 -0500

    Adjust app basic setups

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+const userProfile = {
+  name: "Carlos",
+  phone: "123456789",
+  zona: "Centro",
+  email: "carlos.sierra.at@usa.edu.co",
+  address: "St. Evergreen 456"
+};
+
+function Header() {
+  let message = "";
+  if(userProfile.name !== ""){
+    // TODO message = <h1>Hola {userProfile.name}</h1><br /><div><ViewProfile /></div>;
+    message = <h1>Hola {userProfile.name}</h1>
+  }
+  else
+    message = <h1>Hola Usuario desconocido</h1>
+  
+  return (
+    {message}
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Hola mundo!
       </header>
     </div>
   );
 }
 
-export default App;
+
+
+export {App};
